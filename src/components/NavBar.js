87b813f3a57a39bae7d43815fa30bf3ae0886334
Sara Gilani <saragilani@Saras-MacBook-Pro.local>
2023-01-31 11:51:09 +0000

    changed files around

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
+import logo from './assets/WOAT.jpeg';
 
 const NavBar = () => {
     return ( 
         <header className="header">
-        
         <input className="search" type="search" placeholder="search..." />
         <Link to="">Home</Link>
+        <img src={logo} className="logo" alt="Logo"/>
         <button type="submit"> My profile</button>
         </header>
      );
 }
  
 export default NavBar;
-    <Link to="">Home</Link>
+
