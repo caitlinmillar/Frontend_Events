1f51ab84a805b493b627e74e07cdfdd5f1757da4
Caitlin Millar <caitlin-millar@outlook.com>
2023-02-01 14:08:48 +0000

    made list of events display in rows

--- src/components/ExpandingList.js
+++ src/components/ExpandingList.js
@@ -19,11 +19,11 @@ const ExpandingList = ({ events }) => {
 
     return (
     <>
-    <div className="expandingList">
-        <Collapse forceRender="true"ghost="true"bordered="false" collapsible= "header" onChange={onDropdownChange}>
-            {initialList}
+    <div className="list">
+        <Collapse ghost="true"bordered="false" collapsible= "header" onChange={onDropdownChange}>
+            <div className="initialList">{initialList}</div>
             <Panel  header="show all" key="1">
-                <p>{expandedList}</p>
+                <div className="expandedList">{expandedList}</div>
             </Panel>
         </Collapse>   
     </div>
