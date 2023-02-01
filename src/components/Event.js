d1ad2d7909a72d1c3c8c60ca75bba5cec253fec8
Sara Gilani <saragilani@Saras-MacBook-Pro.local>
2023-02-01 08:58:14 +0000

    adding images to events

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 
+const imageList = [{"id": 1, "url": "https://placebear.com/100/100" }
+,{"id": 2, "url": "src/containers/eventImages/carshow.jpg"}
+]
+
+
 
 const Event = ({event, id}) => (
     <div className="Event">
+         <img src={imageList[1].url}/>
         <button>
             <Link className="show-btn" to={`/events/${event.id}`}>show
-                {/* <img src={carshow} className="logo" alt="event image"/> */}
             </Link>
         </button>
         <p className="eventName">{event.eventName}</p>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,29 @@
 import { Link } from "react-router-dom";
 import logo from './assets/WOAT.jpeg';
+import { MDBBtn, MDBInput, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const NavBar = () => {
     return ( 
         <div className="navbar">
-            <input className="search" type="search" placeholder="search..." />
-            <input className="submit" type="submit"/>
+            <section className=''>
+
+          <form action=''>
+            <MDBRow className='d-flex justify-content-center'>
+
+              <MDBCol md='5' start>
+                <MDBInput contrast type='email' label='Search...' className='mb-4' />
+              </MDBCol>
+
+              <MDBCol size="auto">
+                <MDBBtn outline color='light' type='submit' className='mb-'>
+                  Go
+                </MDBBtn>
+              </MDBCol>
+            </MDBRow>
+          </form>
+        </section>
             <Link to=""> 
                 <img src={logo} className="logo" alt="Logo"/>
             </Link>
