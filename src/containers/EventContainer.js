4bf3ee280cf5bcddc2036b1f5a34f7f954f2747f
Sara Gilani <saragilani@Saras-MacBook-Pro.local>
2023-02-01 12:17:48 +0000

    fixed expand

--- src/containers/EventContainer.js
+++ src/containers/EventContainer.js
@@ -2,10 +2,9 @@ import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import EventList from "../components/EventList";
 import Event from "../components/Event";
-import EventCard from "../components/EventCard";
 import Spotlight from "../components/Spotlight";
 import ExpandingList from "../components/ExpandingList";
-import CarouselList from "../components/CarouselList"
+
 
 
 
