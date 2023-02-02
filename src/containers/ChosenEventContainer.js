92cd779b607d0fc602a3148d4b91cd313da10508
amylianne <amywynn.3.a@gmail.com>
2023-02-02 14:59:22 +0000

    added dropdown search and css

--- src/containers/ChosenEventContainer.js
+++ src/containers/ChosenEventContainer.js
@@ -66,7 +66,7 @@ const ChosenEventContainer = () => {
     {"id": 5, "url": "https://adult.activatelearning.ac.uk/app/uploads/2021/03/gen_news_pottery-and-ceramics-classes-scaled.jpg"},
     {"id": 6, "url": "https://cloudfront-us-east-1.images.arcpublishing.com/sltrib/YWG6CZK4CVCENJEMKWVF63KNHM.JPG"},
     {"id": 7, "url": "https://woollycastle.ie/pub/media/magefan_blog/needlework-4639828_1280_1_.jpg"},
-    {"id": 8, "url": "https://woollycastle.ie/pub/media/magefan_blog/needlework-4639828_1280_1_.jpg"},
+    {"id": 8, "url": "https://swimnorthwest.org/wp-content/uploads/2018/06/79.jpg"},
     {"id": 9, "url": "https://tariqramadan.com/english/wp-content/uploads/sites/9/2016/04/islam-in-europe-full-head-to-hea.jpg"},
     {"id": 10, "url": "https://eu-assets.simpleview-europe.com/conwy2019/imageresizer/?image=%2Fdmsimgs%2FGwrych_Ghost_Tours_2022_558241691.jpg&action=ProductDetailProFullWidth"},
     {"id": 11, "url": "https://pbs.twimg.com/media/FhnnX7EXgAIvFeo.jpg:large"},
